1ff73ea1f25f82f528ecb8fcb11adc8b9351b7e9
IT20617332 <siridana@gmail.com>
2022-07-25 21:01:01 +0530

    stateless functional components added and destructuring arguments done

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,37 @@
 import React, { Component } from "react";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar
-            <span className="badge badge-pill badge-secondary">
-              {this.props.totalCounters}
-            </span>
-          </a>
-        </div>
-      </nav>
-    );
-  }
-}
+//stateless functional component
+const Navbar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Navbar
+          <span className="badge badge-pill badge-secondary">
+            {totalCounters}
+          </span>
+        </a>
+      </div>
+    </nav>
+  );
+};
+//remove this. from {this.props.totalCounters} and add (props) to Navbar function
+
+// class Navbar extends Component {
+//   render() {
+//     return (
+//       <nav className="navbar bg-light">
+//         <div className="container-fluid">
+//           <a className="navbar-brand" href="#">
+//             Navbar
+//             <span className="badge badge-pill badge-secondary">
+//               {this.props.totalCounters}
+//             </span>
+//           </a>
+//         </div>
+//       </nav>
+//     );
+//   }
+// }
 
 export default Navbar;
